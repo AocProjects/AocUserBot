af75a534ab4fd0aac00ec9c18eaa1409852a8469
SakirBey1 <sakirhack81@gmail.com>
2022-10-27 01:53:56 +0300

    Update èvents.py

--- userbot/asisstant/èvents.py
+++ userbot/asisstant/èvents.py
@@ -19,7 +19,7 @@ from telethon import events
 from userbot import tgbot, LOGSPAMMER, PATTERNS
 
 
-def herlock(**args):
+def aoc(**args):
     """ Yeni bir etkinlik kaydedin. """
     pattern = args.get('pattern', None)
     disable_edited = args.get('disable_edited', False)
