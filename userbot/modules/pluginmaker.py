85ea727142fbba73e903752adb550dcbcdbafa06
SakirBey1 <sakirhack81@gmail.com>
2022-11-04 02:00:13 +0300

    Update pluginmaker.py

--- userbot/modules/pluginmaker.py
+++ userbot/modules/pluginmaker.py
@@ -113,5 +113,6 @@ async def _(q):
 
 c_ = c("pluginmaker")
 c_.add_command("pmake", "<pluginin_komutu> <plugin_hızı> <edit/alt/foto/muzik> ", "@aocsupport_bot Sizin1 İçin Bir plugin Oluşturur...")
+c_.add_command("pmake", "deneme 4 edit " , "**bunu bir mesaja yanıt verin**")
 c_.add_info(' Thx to @SakirBey1').add()
 c_.add()
